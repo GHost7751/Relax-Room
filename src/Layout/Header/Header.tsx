b802ef10c9d320d5d939cca76e331c432ca0c00d
Roma <r-ghost@inbox.ru>
2022-06-08 03:26:25 +0300

    Home Page

---
+++ src/Layout/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Relax Room</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me ">
+                         <Link to='/home'><Nav.Link href="#home">Home</Nav.Link></Link> 
+                         <Link to='/movie'> <Nav.Link href="#movie">Movie</Nav.Link></Link>
+                            <Nav.Link href="#series">Series</Nav.Link>
+                            <Nav.Link href="#todos">Todos</Nav.Link>
+                            <Nav.Link href="#weather">Weather</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
